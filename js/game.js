21af90ea49be42e2e27f254a7a7c31be4f1046ab
DJ <epicmrjones@gmail.com>
2023-03-07 21:49:54 -0600

    WIP

---
+++ js/game.js
@@ -0,0 +1,82 @@
+//Welcome to the 201 Coding Practice Game
+let log = console.log;
+
+let student = {};
+student.score = 0;
+
+const quiz = {};
+quiz.functions = nameFun; //10
+quiz.array = arrayFun; //
+quiz.array2 = arrayLoopFun;
+quiz.array3 = arrayLoopBackFun
+quiz.objects = objectFun; //20
+quiz.constructor = constructorFun; //30
+/**/
+quiz.grade = function() {
+    console.clear();
+    log(`Student name: ${student.name} | Grade: ${student.score}`);
+}
+/**/
+quiz.show = function() {
+    console.clear();
+    console.table(this, ['quiz function names', 'Values']);
+}
+
+log('Welcome to the 201 Coding Practice Game');
+
+log('To start, create a variable called name and set it to your name.')
+log('When done, run the testName() function.');
+
+
+function nameFun() {
+    let authorname = 'dij';//'|-| @ ©️ k 3 ®';
+    log(`Create a function that tells me "Hi, my name is ${authorname}!". Make sure your function uses your name variable in the output`);
+    //log(`Hi, my name is ${name}, welcome to BOOTCAMP`);
+    log(`Run testFunction() with your Function-name and Name Variable as parameters to verify correctness`);
+}
+
+function arrayFun() {
+    log('Create an array and name the variable myArray. Fill the array the first name of 5 of your favorite people');
+    log('Use the testArray function to test your array');
+}
+
+function arrayLoopFun() {
+    log('Create a function and use a loop to RETURN the contents of the array as a string');
+    log("To verify, run the testArrayLoop() function with your function's name as a parameter");
+}
+
+function arrayLoopBackFun() {
+    log('Create a function and use a loop to output the contents of the array in reverse');
+    log("To verify, run the testArrayLoopRev() function with your function's name as a parameter");
+}
+
+//create function & string usage
+//create a function that outputs your name
+
+function objectFun() {
+    log('Create an object representation of your favorite pizza and call it myPizza.');
+    log('Required properties include: toppings (array), sauce (string), size (string), crust (string), and cost (int)');
+    log('Set all object properties to lowercase!')
+    //toppings, sauce, size, crust, cost
+}
+
+function constructorFun() {
+    log('Create an object contructor to create multiple kinds of pizza.')
+    log('Also, create an object constructor that can be used to create multiple orders.');
+    log('Required properties for orders include: pizza (array), order status (string) ~ for example: cooking / out for delivery / delivered ~');
+    log('The order objects should also have a method that can update the status of the order');
+    log('Set all object properties to lowercase!')
+}
+
+
+
+
+
+
+
+
+
+
+//, create an array, a loop, an object, an object constructor
+
+
---
+++ js/tests.js
@@ -0,0 +1,118 @@
+function testName() {
+    try {
+        if (name == '') {
+            console.error("You didn't provide a name");
+            student.score -= 10;
+        } else {
+            //'%c TEST FOR multiply() PASSES', 'color: green'
+            log("%c The name you entered is " + name, 'color: green');
+            student.name = name;
+            student.score += 10;
+
+            log('Answer the following problems at your own pace by calling the following quiz functions: ');
+            
+            console.table(quiz);
+        }
+    } catch(e) {
+        log(e);
+        console.error("You didn't create a variable!");
+        student.score -= 10;
+    }
+}
+
+function testFunction(studentFun, name) {
+    if (studentFun(name) == "Hi, my name is " + name + "!") {
+        log("Correct Output")
+        student.score += 10;
+    } else {
+        log("Incorrrect!")
+        student.score -= 10;
+    }
+}
+
+function testArray() {
+    if (typeof myArray != 'object' || myArray.length != 5) {
+        console.error('Your array is invalid');
+        student.score -= 10;
+    } else {
+        log('%c Your array is correct', 'color: green');
+        // let myArrayString = myArray.map()
+        // myArray.map( x => console.log(x))
+    }
+}
+
+function adminStringify(arr) {
+    let str = ''
+    for (let i = 0; i < arr.length; i++) {
+        str += arr[i];
+    }
+    return str;
+}
+function adminStringifyReverse(arr) {
+    let str = ''
+    for (let i = arr.length; i > 0; i--) {
+        str += arr[i];
+    }
+    return str;
+}
+function testArrayLoop(looper) {
+    if (looper() != adminStringify(myArray)) {
+        console.error("Your array function isn't returning the reverse of the myArray variable");
+        student.score -= 10;
+    } else {
+        let output = adminStringify(looper);
+        log("%c Good job! " + output + " is correct output", "color: green");
+    }
+}
+function testArrayLoopRev(looper) {
+    if (looper() != adminStringify(myArray)) {
+        console.error("Your array function isn't returning the reverse of the myArray variable");
+        student.score -= 10;
+    } else {
+        let output = adminStringify(looper);
+        log("%c Good job! " + output + " is correct output", "color: green");
+    }
+}
+
+function testObj(obj = myPizza) {
+    let props = ['toppings', 'sauce', 'size', 'crust', 'cost'];
+    let check = 0;
+    for (let i = 0; i < props.length; i++) {
+        try {
+           if (myPizza[props[i]] != undefined) {
+            check++;
+           }
+            
+        } catch (e) {
+            log(e);
+            console.error("Your pizza object wasn't created correctly");
+        }
+    }
+    if (check == 5) {
+        log('%c Your Pizza object is correct', 'color: green');
+    }
+}
+
+function testOrder(ord) {
+    let props = ['pizza', 'status', 'update'];
+    let check = 0;
+    for (let i = 0; i < props.length; i++) {
+        try {
+           if (ord[props[i]] != undefined && typeof ord[props[2]] == 'function') {
+            check++;
+           }
+        } catch (e) {
+            log(e);
+            console.error("Your order object wasn't created correctly");
+        }
+    }
+    if (check == 3) {
+        log('%c Your order object is correct', 'color: green');
+    }
+}
+
+function testConstructors(piz, ord, ) {
+    testObj(piz);
+    testOrder(ord);
+    
+}
