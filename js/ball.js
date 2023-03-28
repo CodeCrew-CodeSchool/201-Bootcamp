f910043e8633003243f0f0f306b12501d20103c5
DJ <epicmrjones@gmail.com>
2023-03-28 10:37:52 -0500

    animating ball

--- js/ball.js
+++ js/ball.js
@@ -1,54 +1,34 @@
-// mods by Patrick OReilly 
-// Twitter: @pato_reilly Web: http://patricko.byethost9.com
+var config = {
+    type: Phaser.AUTO,
+    width: 300,
+    height: 200,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
 
-var game = new Phaser.Game(200, 200, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(config);
 
-function preload() {
+function preload ()
+{
 
-    //  You can fill the preloader with as many assets as your game requires
-
-    //  Here we are loading an image. The first parameter is the unique
-    //  string by which we'll identify the image later in our code.
-
-    //  The second parameter is the URL of the image (relative)
-    game.load.image('ball', '../assets/ball01.png');
-    console.log('loaded')
+    this.load.image('ball', 'assets/ball01.png');
 }
 
-var image;
-
-function create() {
-
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    //  This creates a simple sprite that is using our loaded image and
-    //  displays it on-screen
-    //  and assign it to a variable
-    image = game.add.sprite(0, 0, 'ball');
-
-    game.physics.enable(image, Phaser.Physics.ARCADE);
+function create ()
+{
     
-    //  This gets it moving
-    image.body.velocity.setTo(200,200);
-    
-    //  This makes the game world bounce-able
-    image.body.collideWorldBounds = true;
-    
-    //  This sets the image bounce energy for the horizontal 
-    //  and vertical vectors. "1" is 100% energy return
-    image.body.bounce.set(1);
+    var logo = this.physics.add.image(400, 100, 'ball');
 
-}
-
-function update () {
-
-    //nothing required here
-    
-}
-
-function render () {
-
-    //debug helper
-    game.debug.spriteInfo(image,32,32);
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
 
 }
